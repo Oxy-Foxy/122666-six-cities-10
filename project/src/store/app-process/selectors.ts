395e976302362f71ac431eea90ef249cc5168dc7
Oxana Garascenco <oxana.garashcenco@gmail.com>
2022-08-18 19:39:42 +0300

    fixed popular sort

--- project/src/store/app-process/selectors.ts
+++ project/src/store/app-process/selectors.ts
@@ -3,6 +3,7 @@ import {State} from '../../types/state';
 import {sortTypes} from '../../const';
 import {getOffers} from '../data-process/selectors';
 import { createSelector } from 'reselect';
+import { cloneDeep } from 'lodash';
 
 export const getCurrentCity = (state: State): string => state[NameSpace.App].city;
 export const getSortType = (state: State): string => state[NameSpace.App].sortType;
@@ -15,15 +16,16 @@ export const filterOffers = createSelector(
 export const sortOffers = createSelector(
   [filterOffers, getSortType],
   (offers, sortType) => {
+    const filteredOffers = cloneDeep(offers);
     switch (sortType) {
       case sortTypes.Popular:
         return offers;
       case sortTypes.LowToHight:
-        return offers.sort((a,b) => a.price - b.price);
+        return filteredOffers.sort((a,b) => a.price - b.price);
       case sortTypes.HighToLow:
-        return offers.sort((a,b) => b.price - a.price);
+        return filteredOffers.sort((a,b) => b.price - a.price);
       case sortTypes.TopRated:
-        return offers.sort((a,b) => b.rating - a.rating);
+        return filteredOffers.sort((a,b) => b.rating - a.rating);
       default:
         return offers;
     }
